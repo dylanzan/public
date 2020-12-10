1cb2c59b985bc6e89a1f41e3e77cee347e8bf9ff
zanshichun <zsc9602@163.com>
2020-12-10 15:44:59 +0800

    first commit

---
+++ src/safequeue/SliceQueue.go
@@ -0,0 +1,32 @@
+package safequeue
+
+import "sync"
+
+type SliceQueue struct {
+	data []interface{}
+	mu sync.Mutex
+}
+
+func NewSliceQueue(n int) (q *SliceQueue) {
+	return &SliceQueue{data: make([]interface{},0,n)}
+}
+
+//Enqueue 值入队尾
+func (q *SliceQueue)Enqueue(v interface{})  {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	q.data=append(q.data,v)
+}
+
+func (q *SliceQueue)Dequeue() interface{}{
+	q.mu.Lock()
+	if len(q.data)==0 {
+		q.mu.Unlock()
+		return nil
+	}
+
+	v:=q.data[0]
+	q.data=q.data[1:]
+	q.mu.Unlock()
+	return v
+}
